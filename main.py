0de72a4587dc9d98029673d2493da49ef5a590f6
Alisonsantos77 <Alisondev77@hotmail.com>
2024-04-30 17:29:45 -0300

    :art: update grid layout :zap: improvement of functions

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # importando Custom Tkinter
 import customtkinter as ctk
 from tkinter import filedialog
-
+import time
+import os
 # Importando PyTube
 from pytube import YouTube
-
-from tkinter import font, Tk
 
 
 # Cores ----------------------------------------------------------------
@@ -20,34 +19,41 @@ rose_ebony = '#513b3c'
 # Criando a janela
 janela = ctk.CTk()
 janela.title("PYT")
-janela.geometry('500x300')
+janela.geometry('250x300')
 
-#  Funções
 
 # Variável global para armazenar o diretório selecionado
 select_dir = None
 
+#  Funções
 
 def new_download():
     global select_dir
+    # Obtém a URL do vídeo do campo link_input
     url = link_input.get()
+    # Cria um objeto YouTube com a URL
     yt = YouTube(url)
-
+    # Seleciona o stream de vídeo com a resolução mais alta
     video_stream = yt.streams.get_highest_resolution()
 
     # Vai usar a variavel select_dir caso já tenha sido selecionada
     if select_dir:
-        video_stream.download(select_dir)
+        base_name = os.path.splitext(video_stream.default_filename)[0]
+        video_stream.download(select_dir, filename=f'{base_name}.mp4')
         print("Download Completo")
+        print("Insira um novo link")
     else:
-        # chama a função save_dir para obter o diretório
+        # caso contrario, chama a função save_dir para obter o diretório
         select_dir = save_dir()
         if select_dir:
-            video_stream.download(select_dir)
+            base_name = os.path.splitext(video_stream.default_filename)[0]
+
+            video_stream.download(select_dir, filename=f'{base_name}.mp4')
             print("Download Completo")
         else:
             print('Selecione um diretório')
-
+    # Limpa o campo de entrada após o download
+    link_input.delete(0, 'end')
 
 def save_dir():
     global select_dir  # Acessa a variável global
@@ -62,52 +68,49 @@ def save_dir():
 
 def new_mp3():
     global select_dir
-    input_mp3 = ctk.CTkInputDialog(text="Baixar em MP3", title="PYT MP3")
+    input_mp3 = ctk.CTkInputDialog(text="Somente audio", title="PYT MP3")
 
     audio_url = input_mp3.get_input()
     yt = YouTube(audio_url)
-
+    # Filtra para buscar somente o primeiro audio
     filter_mp3 = yt.streams.filter(only_audio=True).first()
 
     if select_dir:
-        filter_mp3.download(select_dir)
+        # Remove a extensão do nome do arquivo original e adiciona o sufixo
+        base_name = os.path.splitext(filter_mp3.default_filename)[0]
+        unique_filename = f"{base_name}_mp3_{int(time.time())}.mp3"
+        filter_mp3.download(select_dir, filename=unique_filename)
         print("Download Completo")
     else:
         select_dir = save_dir()
         if select_dir:
-            filter_mp3.download(select_dir)
+            # Remove a extensão do nome do arquivo original e adiciona o sufixo
+            base_name = os.path.splitext(filter_mp3.default_filename)[0]
+            unique_filename = f"{base_name}_mp3_{int(time.time())}.mp3"
+            filter_mp3.download(select_dir, filename=unique_filename)
             print("Download Completo")
         else:
             print('Selecione um diretório')
+            
+    # Limpa o campo de entrada após o download
+    link_input.delete(0, 'end')
 
 # Widgets
-
-
 texto = ctk.CTkLabel(janela, text='Baixe aqui',
-                     font=ctk.CTkFont(family='roboto', size=16))
-texto.pack(padx=20, pady=20, anchor='center')
+                    font=ctk.CTkFont(family='roboto', size=16))
+texto.grid(row=0, column=0, columnspan=2, padx=20, pady=20, sticky='n')
 
 link_input = ctk.CTkEntry(janela, placeholder_text='Seu link')
-link_input.pack(padx=20, pady=20, anchor='center')
+link_input.grid(row=1, column=0, columnspan=2, padx=20, pady=20, sticky='ew')
 
-# check_mp3 = ctk.CTkCheckBox(janela, text='.MP3')
-# check_mp3.pack(padx=(0,  10), pady=10, anchor='center')
+# Criando e organizando os botões em uma grade
+botao_format = ctk.CTkButton(janela, text='Formatos', command=new_mp3, fg_color=azul_escuro, text_color=amarelo, width=5)
+botao_format.grid(row=2, column=0, padx=30, pady=30)
 
-# check_mp4 = ctk.CTkCheckBox(janela, text='.MP4')
-# check_mp4.pack( padx=(10,  0), pady=0, anchor='center')
+botao_dir = ctk.CTkButton(janela, text='Diretório', width=5, fg_color=branco, text_color=azul_escuro, command=save_dir, font=ctk.CTkFont(family='roboto', size=12))
+botao_dir.grid(row=2, column=1, padx=30, pady=30)
 
-botao_format = ctk.CTkButton(
-    janela, text='Formatos', command=new_mp3, fg_color=azul_escuro, text_color=amarelo, width=5)
-botao_format.pack(padx=10, pady=0, anchor='center')
-
-
-botao_dir = ctk.CTkButton(janela, text='Diretório', width=5,
-                          fg_color=branco, text_color=azul_escuro, command=save_dir, font=ctk.CTkFont(family='roboto', size=12))
-botao_dir.pack(side='top', padx=(0, 30), pady=0, anchor='se')
-
-botao_submit = ctk.CTkButton(
-    janela, text='Baixar', command=new_download, font=ctk.CTkFont(family='roboto', size=16))
-botao_submit.pack(padx=20, pady=2, anchor='center')
-
+botao_submit = ctk.CTkButton(janela, text='Baixar', command=new_download, font=ctk.CTkFont(family='roboto', size=16))
+botao_submit.grid(row=3, column=0, columnspan=2, padx=20, pady=30)
 
 janela.mainloop()
